e20b7bae4598c3d747dee720b07879d520d3f025
edobasky <edore7@gmail.com>
2022-04-23 12:02:11 -0700

    shopping cart feature added

--- shoppi_app/src/Pages/Cart.jsx
+++ shoppi_app/src/Pages/Cart.jsx
@@ -3,6 +3,7 @@ import styled from 'styled-components'
 import {Announcement} from "../components/Announcement"
 import Footer from "../components/Footer"
 import Navbar from "../components/Navbar"
+import {IoMdAdd,IoMdRemove} from "react-icons/io"
 
 const Cart = () => {
   return (
@@ -25,20 +26,60 @@ const Cart = () => {
                 <Info>
                     <Product>
                         <ProductDetail>
-                            <Image  src="https://thumbs.dreamstime.com/b/young-people-making-circle-showing-their-sports-shoes-outdoors-above-view-young-people-making-circle-showing-their-sports-156844682.jpg"/>
+                            <Image  src="https://hips.hearstapps.com/vader-prod.s3.amazonaws.com/1614188818-TD1MTHU_SHOE_ANGLE_GLOBAL_MENS_TREE_DASHERS_THUNDER_b01b1013-cd8d-48e7-bed9-52db26515dc4.png?crop=1xw:1.00xh;center,top&resize=480%3A%2A"/>
                             <Details>
-                                <ProductName><b>Product:</b> JESSIE THUNDER SHOES</ProductName>
+                                <ProductName><b>Product:</b> JESSIE AIRMAX SHOES</ProductName>
                                 <ProductId><b>ID:</b> 93813718293</ProductId>
-                                <ProductColor />
+                                <ProductColor color="black" />
                                 <ProductSize><b>Size:</b> 37.5</ProductSize>
                             </Details>
                         </ProductDetail>
                         <PriceDetail>
-                            Price
+                            <ProductAmountContainer>
+                                <IoMdAdd />
+                                <ProductAmount>2</ProductAmount>
+                                <IoMdRemove />
+                            </ProductAmountContainer>
+                            <ProductPrice>$ 30</ProductPrice>
+                        </PriceDetail>
+                    </Product>
+                    <Hr />
+                    <Product>
+                        <ProductDetail>
+                            <Image  src="https://i.pinimg.com/474x/97/39/55/973955d3d9d677575872da1fcb52172d.jpg"/>
+                            <Details>
+                                <ProductName><b>Product:</b> MINATO TSHIRT</ProductName>
+                                <ProductId><b>ID:</b> 93813718293</ProductId>
+                                <ProductColor color="gray" />
+                                <ProductSize><b>Size:</b> M</ProductSize>
+                            </Details>
+                        </ProductDetail>
+                        <PriceDetail>
+                            <ProductAmountContainer>
+                                <IoMdAdd />
+                                <ProductAmount>2</ProductAmount>
+                                <IoMdRemove />
+                            </ProductAmountContainer>
+                            <ProductPrice>$ 20</ProductPrice>
                         </PriceDetail>
                     </Product>
                 </Info>
-                <Summary>summary</Summary>
+                <Summary>
+                    <SummaryTitle>ORDER SUMMARY</SummaryTitle>
+                    <SummaryItem>
+                        <SummaryItemText>Estimated Shipping</SummaryItemText>
+                        <SummaryItemPrice>$ 5.90</SummaryItemPrice>
+                    </SummaryItem>
+                    <SummaryItem>
+                        <SummaryItemText>Shipping Discount</SummaryItemText>
+                        <SummaryItemPrice>$ -5.90</SummaryItemPrice>
+                    </SummaryItem>
+                    <SummaryItem type="total">
+                        <SummaryItemText>Total</SummaryItemText>
+                        <SummaryItemPrice>$ 80</SummaryItemPrice>
+                    </SummaryItem>
+                    <Button>CHECKOUT NOW</Button>
+                </Summary>
             </Bottom>
         </Wrapper>
         <Footer />
@@ -89,21 +130,90 @@ const Info = styled.div`
     flex:3;
 `
 
-const Product = styled.div``
-const ProductDetail = styled.div``
-const Image = styled.img``
-const Details = styled.span``
+const Product = styled.div`
+    display:flex;
+    justify-content:space-between;
+`
+const ProductDetail = styled.div`
+    flex:2;
+    display:flex;
+`
+const Image = styled.img`
+    width:200px;
+`
+const Details = styled.div`
+    padding:20px;
+    display:flex;
+    flex-direction:column;
+    justify-content:space-around;
+`
 const ProductName = styled.span``
 const ProductId = styled.span``
-const ProductColor = styled.div``
+const ProductColor = styled.div`
+    width:20px;
+    height:20px;
+    border-radius:50%;
+    background-color:${(props) => props.color};
+`
 const ProductSize = styled.span``
-const PriceDetail = styled.span``
+const PriceDetail = styled.div`
+    flex:1;
+    display:flex;
+    align-items:center;
+    justify-content:center;
+    flex-direction:column;
+`
 
+const ProductAmountContainer = styled.div`
+    display:flex;
+    align-items:center;
+    margin-bottom:20px;
+`
+const ProductAmount = styled.div`
+    font-size:24px;
+    margin:5px;
+`
+const ProductPrice = styled.div`
+    font-size:30px;
+    font-weight:200;
+`
+
+const Hr = styled.hr`
+    background-color:#eee;
+    border:none;
+    height:1px;
+`
 
 
 const Summary = styled.div`
     flex:1;
+    border:0.5px solid lightgrey;
+    border-radius: 10px;
+    padding: 20px;
+    height:50vh;
+`
+
+const SummaryTitle = styled.h1`
+    font-weight:200;
     
+`
+const SummaryItem = styled.div`
+    margin:30px 0px;
+    display:flex;
+    justify-content:space-between;
+    font-weight: ${props => props.type === "total" && "500"};
+    font-size: ${props => props.type === "total" && "600"}
+`
+const SummaryItemText = styled.span``
+const SummaryItemPrice = styled.span``
+const Button = styled.button`
+    width:100%;
+    padding:10px;
+    background-color:black;
+    color:white;
+    font-weight:600;
+    border-radius:5px;
+    border:none;
 `
 
 export default Cart
